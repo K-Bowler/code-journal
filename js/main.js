9c67cab62428c16def62a5333175c05caf281443
K-Bowler <Nebulasends@gmail.com>
2022-04-04 22:47:16 +0000

    user-can-create-an-entry WIP#2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 /* global data */
 /* exported data */
+
+var $imgInput = document.querySelector('#input-img');
+var $img = document.querySelector('.entry-img');
+var $saveBtn = document.querySelector('.save-btn');
+
+function photoUrlInput(event) {
+  var $imgUrl = document.querySelector('#input-img').value;
+  $img.setAttribute('src', $imgUrl);
+}
+$imgInput.addEventListener('input', photoUrlInput);
+
+function saveContent() {
+
+}
+$saveBtn.addEventListener('click', saveContent);
